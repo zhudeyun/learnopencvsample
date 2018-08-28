8b7e43e5985d106bff3ba5f0fad7fc15e71e77d0
zhudeyun <publicregister01@163.com>
2018-08-28 16:59:58 +0800

    add HelloEmgu

---
+++ HelloEmgu/Form1.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace HelloEmgu
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            this.histogramBox1 = new Emgu.CV.UI.HistogramBox();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.imageBox1.Location = new System.Drawing.Point(33, 23);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(494, 352);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
+            // 
+            // histogramBox1
+            // 
+            this.histogramBox1.BackColor = System.Drawing.SystemColors.ActiveBorder;
+            this.histogramBox1.Location = new System.Drawing.Point(33, 381);
+            this.histogramBox1.Name = "histogramBox1";
+            this.histogramBox1.Size = new System.Drawing.Size(494, 139);
+            this.histogramBox1.TabIndex = 3;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(579, 54);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(841, 575);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.histogramBox1);
+            this.Controls.Add(this.imageBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Emgu.CV.UI.ImageBox imageBox1;
+        private Emgu.CV.UI.HistogramBox histogramBox1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ HelloEmgu/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.UI;
+
+namespace HelloEmgu
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            
+
+            InitializeComponent();
+
+
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+            ////创建一个显示窗口
+            //CvInvoke.NamedWindow("Lena", Emgu.CV.CvEnum.NamedWindowType.AutoSize);
+            ////显示图像
+            //CvInvoke.Imshow("Lena", mat);
+
+            ////等等按键输入
+            //CvInvoke.WaitKey(0);
+            ////销毁显示窗口
+            //CvInvoke.DestroyWindow("Lena");
+
+            ////释放资源
+            //mat.Dispose();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //从文件中读取图像
+            var mat = new Mat("lena.jpg");
+            imageBox1.Image = mat;
+            //1.HistogramBox
+            histogramBox1.GenerateHistograms(mat, 32);
+            histogramBox1.Refresh();
+            histogramBox1.Show();
+
+            //2.使用HistogramViewer
+            HistogramViewer.Show(mat, 32); //image[0] 顯示Blue,bin = 32
+        }
+    }
+}
